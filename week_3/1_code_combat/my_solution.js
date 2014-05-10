e21f41ca1d4668ee15ce7a2e4777dd9e87a48099
bajunio <bajunio@gmail.com>
2014-05-09 18:41:58 -0700

    week 3 challenge 1 js added

--- week_3/1_code_combat/my_solution.js
+++ week_3/1_code_combat/my_solution.js
@@ -1,16 +1,41 @@
-// I worked on this challenge [by myself, with:]
- 
-// For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
-// Note: to make commenting easier, you can highlight the section you want to comment and hold 
-// command + / This will comment the line. 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-// Reflection:
-// Write your reflection here.
+
+
+
+
+
+
+// Challenge 2
+
+// Tharin might consider taking a detour after line 3.
+
+this.moveUp();
+// move right collect mushroom
+this.moveRight();
+// move left back to path
+this.moveLeft();
+// move forward to engage
+this.moveUp();
+// engage
+this.attackNearbyEnemy();
+
+// Challenge 3
+
+// move right 1
+// attack ogre
+// move right 1
+// move down 1
+// collect pot
+// move up 1
+// move right 1
+// attack ogre
+
+this.moveRight();
+this.attackNearbyEnemy();
+this.moveRight();
+this.moveDown();
+this.moveUp();
+this.moveRight();
+this.attackNearbyEnemy();
+
+// Challenge 4
+
